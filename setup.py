befd4898165d751df8511994458d89f3e788d018
keming <kemingy94@gmail.com>
2019-12-01 16:59:32 +0800

    add badges, publish to pypi

--- setup.py
+++ setup.py
@@ -13,11 +13,11 @@ with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
 
 
 setup(
-    name='',
+    name='spectree',
     version='0.0.1',
     author='Keming Yang',
     author_email='kemingy94@gmail.com',
-    description='',
+    description='generate OpenAPI document and validate request & response with Python annotations.',
     long_description=readme,
     long_description_content_type='text/markdown',
     url='https://github.com/0b01001001/spectree',
@@ -25,7 +25,13 @@ setup(
     package_data={
     },
     classifiers=[
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Framework :: flask',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     install_requires=requires,
     zip_safe=False,
