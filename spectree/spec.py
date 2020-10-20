6d2ebdaf623dcec9b611f8e8e99a3e77f2064c06
Natasha Aleksandrova <natasha.aleksandrova@zonarsystems.com>
2020-10-20 10:18:06 -0700

    cleanup

--- spectree/spec.py
+++ spectree/spec.py
@@ -163,7 +163,6 @@ class SpecTree:
 
                 routes[path][method.lower()] = {
                     'summary': summary or f'{name} <{method}>',
-                    #'operationId': f'{name}__{method.lower()}',
                     'operationId': f'{method.lower()}_{path}',
                     'description': desc or '',
                     'tags': getattr(func, 'tags', []),
