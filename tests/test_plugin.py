3561aa1a43987df293331b53afcb0aa76a70177e
keming <kemingy94@gmail.com>
2020-10-26 10:48:15 +0800

    add operationId test

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -21,6 +21,7 @@ def test_plugin_spec(api):
     assert ping['parameters'][0]['in'] == 'header'
     assert ping['summary'] == 'summary'
     assert ping['description'] == 'description'
+    assert ping['operationId'] == 'get_/ping'
 
     user = api.spec['paths']['/api/user/{name}']['post']
     assert user['tags'] == ['api', 'test']
