41fb4a8f58dbfdeec1343e4d075fbd3a42309775
Keming <kemingy94@gmail.com>
2021-04-24 16:09:46 +0800

    support tag with description and external docs

--- spectree/__init__.py
+++ spectree/__init__.py
@@ -1,9 +1,10 @@
 import logging
 
+from .models import Tag
 from .response import Response
 from .spec import SpecTree
 
-__all__ = ["SpecTree", "Response"]
+__all__ = ["SpecTree", "Response", "Tag"]
 
 # setup library logging
 logging.getLogger(__name__).addHandler(logging.NullHandler())
