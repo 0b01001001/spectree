fd1f6fbab41be3a19ea1525ec85cd2410c042d41
Keming <kemingy94@gmail.com>
2022-02-21 23:30:08 +0800

    release 0.7.5

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, "requirements.txt"), encoding="utf-8") as f:
 
 setup(
     name="spectree",
-    version="0.7.4",
+    version="0.7.5",
     license="Apache-2.0",
     author="Keming Yang",
     author_email="kemingy94@gmail.com",
