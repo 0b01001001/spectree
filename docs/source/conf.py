210fafce05391a8884307c5ac6cdb9c340ec0037
Keming <kemingy94@gmail.com>
2022-05-18 00:23:41 +0800

    use 'furo' as the new doc theme (#223)
    use 'furo' as doc theme
    
    Signed-off-by: Keming <kemingy94@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,14 +12,15 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../'))
+
+sys.path.insert(0, os.path.abspath("../../"))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'spectree'
-copyright = '2020, Keming Yang'
-author = 'Keming Yang'
+project = "spectree"
+copyright = "2020, Keming Yang"
+author = "Keming Yang"
 
 
 # -- General configuration ---------------------------------------------------
@@ -28,13 +29,13 @@ author = 'Keming Yang'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.viewcode',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.githubpages',
+    "sphinx.ext.viewcode",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.githubpages",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -47,12 +48,12 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "furo"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # read the doc
-master_doc = 'index'
+master_doc = "index"
