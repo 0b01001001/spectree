15f87ccc175097a38c04dcd6f2dc803cdb1468e9
Dmitry Marchuk <dmarchuk@remedypointsolutions.com>
2022-08-04 10:25:21 +0200

    Add UTests for quart plugin

---
+++ tests/quart_imports/__init__.py
@@ -0,0 +1,17 @@
+from .dry_plugin_quart import (
+    test_quart_doc,
+    test_quart_no_response,
+    test_quart_return_model,
+    test_quart_skip_validation,
+    test_quart_validate,
+    test_quart_validation_error_response_status_code,
+)
+
+__all__ = [
+    "test_quart_return_model",
+    "test_quart_skip_validation",
+    "test_quart_validation_error_response_status_code",
+    "test_quart_doc",
+    "test_quart_validate",
+    "test_quart_no_response",
+]
