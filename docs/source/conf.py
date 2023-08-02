a89e55c882712909f91439627a10121a0b8008bc
Keming <kemingy94@gmail.com>
2023-08-02 10:37:04 +0800

    chore: fix doc css
    Signed-off-by: Keming <kemingy94@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -72,6 +72,9 @@ html_context = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_css_files = [
+  'custom.css',
+]
 
 # read the doc
 master_doc = "index"
