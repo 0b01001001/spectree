bce9963c7e03bcb37b58a23e7ddf1a8ffc3ae37b
Keming <kemingy94@gmail.com>
2023-08-02 10:38:11 +0800

    chore: fix doc css (#334)
    * chore: fix doc css
    
    Signed-off-by: Keming <kemingy94@gmail.com>
    
    * format
    
    Signed-off-by: Keming <kemingy94@gmail.com>
    
    ---------
    
    Signed-off-by: Keming <kemingy94@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -60,7 +60,7 @@ html_theme_options = {
             "title": "Sponsor me",
             "url": "https://github.com/sponsors/kemingy",
         },
-    ]
+    ],
 }
 html_context = {
     "source_type": "github",
@@ -72,6 +72,7 @@ html_context = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_css_files = ["custom.css"]
 
 # read the doc
 master_doc = "index"
