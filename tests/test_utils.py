4a7ee3151bdbf03a42fad55340973c7f4a461efb
Alex Ted <alextedx@gmail.com>
2026-08-30 13:05:48 +0300

    fix: type checking v2 in tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -427,8 +427,9 @@ def test_get_request_model_hints_unresolvable_return_annotation():
 
 
 def test_get_request_model_hints_unresolvable_parameter_annotation():
-    def func(json: "CompletelyNonExistentType") -> int:  # noqa: F821# type: ignore
+    def func(json: "DemoModel") -> int:  # noqa: F821
         raise NotImplementedError
+    func.__annotations__["json"] = "CompletelyNonExistentType"
 
     with pytest.raises(NameError):
         get_request_model_hints(func)
